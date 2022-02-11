1e4b2624e5ce8305b2dea410150a6639043cb6d8
Blue Rebel <kevinwigen@gmail.com>
2022-02-11 19:56:53 +0100

    Update 1.2

--- config.lua
+++ config.lua
@@ -1,5 +1,10 @@
 Config = {}
 
+Config.Create_Code_Command = "createp" -- The command used to create a promo code
+Config.Redeem_Code_Command = "redeemp" -- The command used to redeem a promo code
+
+Config.Garage_Version = "new" -- Put this to new if you use the newest garage version
+
 Config['Webhooks'] = {
 	["Redeemed_Code"] = "",
 	["Created_Code"] = ""
