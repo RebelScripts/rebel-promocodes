a2ac1093afd01df8bd73c69a7dcc00720e1168f9
Blue Rebel <kevinwigen@gmail.com>
2022-02-14 01:10:26 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 author 'Blue_Rebel | Blue Rebel#3614'
 description 'Simple promo code script made by Rebel Scripts | https://discord.gg/YJ3YzNjWDy'
-version '1.2'
+version '1.3'
 
 shared_scripts {
 	'config.lua',
